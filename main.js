b6d925431316666e758b722569535f6c66357e20
CaioBuard <buardcaio@hotmail.com>
2023-06-28 20:01:59 -0300

    Playing sound when clicking on button POM

---
+++ main.js
@@ -0,0 +1,38 @@
+function playSoundPOM () {
+    document.querySelector('#som_tecla_pom').play();
+} 
+
+function playSoundCLAP() {
+    document.querySelector('#som_tecla_clap').play()
+}
+
+function playSoundTIM() {
+    document.querySelector('#som_tecla_tim').play()
+}
+
+function playSounPUFF() {
+    document.querySelector('#som_tecla_puff').play()
+}
+
+function playSoundSPLASH() {
+    document.querySelector('#som_tecla_SPLASH').play()
+}
+
+function playSoundTOIM() {
+    document.querySelector('#som_tecla_TOIM').play()
+}
+
+function playSoundPSH() {
+    document.querySelector('#som_tecla_psh').play()
+}
+
+function playSoundTIC() {
+    document.querySelector('#som_tecla_tic').play()
+}
+
+function playSoundTOM() {
+    document.querySelector('#som_tecla_tom').play()
+}
+
+document.querySelector ('.tecla_pom').onclick = playSoundPOM;
+
